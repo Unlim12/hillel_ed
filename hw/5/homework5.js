5e2b4e4c04ab4b1161df106f4570fb176b3ddc2f
Unlim12 <69985150+Unlim12@users.noreply.github.com>
2022-09-30 17:14:23 +0300

    Delete homework5.js

--- hw/5/homework5.js
+++
@@ -1,7 +0,0 @@
-function func() {
-    let num1 = Number(document.getElementById("num1").value);
-    let num2 = Number(document.getElementById("num2").value);
-    let num3 = Number(document.getElementById("num3").value);
-    let num4 = num1 + num2 + num3;
-    alert(num4 / 3);
-}
