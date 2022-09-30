9eed074f50d0340e16f7a2c194e349b4d21ed21a
Unlim12 <69985150+Unlim12@users.noreply.github.com>
2022-09-30 17:14:35 +0300

    Delete homework2.js

--- hw/homework2.js
+++
@@ -1,9 +0,0 @@
-    let x1 = Number(window.prompt("Type a number", ""));
-    let x2 = Number(window.prompt("Type a number", ""));
-
-    console.log(x1 + x2);
-    console.log(x1 - x2);
-    console.log(x1 / x2);
-    console.log(x1 * x2);
-    console.log(x1 % x2);
-    console.log(x1 ** x2);
